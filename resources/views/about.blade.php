44c089857081db6335456d6ad2a325a65351326d
Khan Solomon <63509669+solomonya@users.noreply.github.com>
2023-02-07 16:53:32 +0000

    added picture

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,5 +4,6 @@
         <p>
             Величайший IOS разработчик, и легенда этого времени. Ещё всем вру, что не умею играть в настольный теннис.
         </p>
+        <img alt="some picture" src="https://images.unsplash.com/photo-1675725564198-a33fceec9433?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
     </div>
 </x-layout>
